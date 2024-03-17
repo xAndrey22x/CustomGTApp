8b1d003ab53fa25fb89ed9753ec669c11966e6bb
Istrate Andrei <andrei_istrate20@yahoo.ro>
2024-03-18 01:00:21 +0200

    root

---
+++ customGTApp/src/main/java/com/customGTApp/CustomGtAppApplication.java
@@ -0,0 +1,13 @@
+package com.customGTApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomGtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomGtAppApplication.class, args);
+	}
+
+}
---
+++ customGTApp/src/test/java/com/customGTApp/CustomGtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customGTApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomGtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
